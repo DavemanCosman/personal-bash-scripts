2e465bbcf6442b845eb2eb25e3e38bfdf052823f
David Cosman <davemancosman@gmail.com>
2023-09-21 09:37:47 -0400

    Update VSCode_gitlab.sh
    updated instructions for how to use

--- VSCode_gitlab.sh
+++ VSCode_gitlab.sh
@@ -2,14 +2,14 @@
 
 #compile this program using chmod +x ~/VSCode_gitlab.sh
 currentdir=$(pwd)
-targetdir='Documents/Repositories/Repositories - GitLab/'
+targetdir='Desktop/Repositories/Repositories - GitLab/'
 LBLUE='\033[1;34m'
 RED='\033[1;31m'
 YELLOW='\033[1;33m'
 CYAN='\033[0;36m'
 NC='\033[0m'
 
-#replace the $targetdir path with your own machines, where repos are under
+#replace the $targetdir path with your own machines, where your gitlab repos are under
 if [ -z "$SSH_AUTH_SOCK" ] && [[ $currentdir == *$targetdir* ]] ; then
     echo -e "${CYAN}Connecting to ssh-agent\r"
     eval `ssh-agent -s`
@@ -22,7 +22,9 @@ else
 	echo -e "> ${RED}****Warning****\r${NC}
 > Current directory is not a Git repository. Please rerun this bash program\r
   in a Git repository path to run the connection agent for your session.\r
-> ${CYAN}Run the following commands:${NC}\r
+> 1. close VSCode,\r
+> 2. start a bash terminal from the desktop, and\r
+> 3. ${CYAN}run the following commands:${NC}\r
 > cd ${YELLOW}\"$targetdir${LBLUE}{your repository}${YELLOW}\"${NC}\r
 > ${YELLOW}~/VSCode_gitlab.sh${NC}"
 fi
